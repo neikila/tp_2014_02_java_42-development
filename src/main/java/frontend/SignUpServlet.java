dca4779a7cda2ff6fae6c8635ec3d78a0d09473a
Kirill <neikila@gmail.com>
2015-02-22 16:12:54 +0300

    Initial commit

---
+++ src/main/java/frontend/SignUpServlet.java
@@ -0,0 +1,55 @@
+package frontend;
+
+import main.AccountService;
+import main.UserProfile;
+import templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by v.chibrikov on 13.09.2014.
+ */
+public class SignUpServlet extends HttpServlet {
+    private AccountService accountService;
+
+    public SignUpServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String pageTml = "signupstatus.html";
+        Map<String, Object> pageVariables = new HashMap<>();
+        if (accountService.isUserExist(name)) {
+            pageVariables.put("signUpStatus", "User with name: " +
+                    "" + name + " already exists");
+        } else {
+            pageTml = "signUpForm.html";
+        }
+        response.getWriter().println(PageGenerator.getPage(pageTml, pageVariables));
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    public void doPost(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        Map<String, Object> pageVariables = new HashMap<>();
+        if (accountService.addUser(login, new UserProfile(login, password, ""))) {
+            pageVariables.put("signUpStatus", "New user created");
+        } else {
+            pageVariables.put("signUpStatus", "User with login: " +
+                    "" + login + " already exists");
+        }
+        response.getWriter().println(PageGenerator.getPage("signupstatus.html", pageVariables));
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
