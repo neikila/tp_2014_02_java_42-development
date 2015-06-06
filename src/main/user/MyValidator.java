88acd2e8963af57a2dcff42ea771664bc26278c8
Kirill <neikila@gmail.com>
2015-06-06 08:49:50 +0300

    2 symbols for start

--- src/main/user/MyValidator.java
+++ src/main/user/MyValidator.java
@@ -1,12 +1,13 @@
 package main.user;
 
+import org.apache.commons.validator.EmailValidator;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.apache.commons.validator.EmailValidator;
 
 public class MyValidator {
     private static MyValidator myValidator;
-    private static Pattern usernamePattern = Pattern.compile("^\\w{5,63}$");
+    private static Pattern usernamePattern = Pattern.compile("^\\w{2,63}$");
     private static Pattern passwordPattern = Pattern.compile("^\\w{6,20}$");
 
     public static boolean isUserNameValid(String userName) {
